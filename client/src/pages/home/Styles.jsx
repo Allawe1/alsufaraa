0c68b2d5ccf631800306187afdd9dea1ba539608
Ali <ali.mohammed4@yahoo.com>
2021-10-13 13:42:04 +0300

    ignore npm-debug

--- client/src/pages/home/Styles.jsx
+++ client/src/pages/home/Styles.jsx
@@ -22,7 +22,7 @@ export const useStyles = makeStyles((theme) => ({
      },
      [theme.breakpoints.only('xs')]: {
       padding : '80px 10px',
-      marginTop : 135
+      marginTop : 145
      },
   },
   Home2: {
