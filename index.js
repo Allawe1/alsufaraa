1527d98d995b7750319fb625d669a9632e30a729
Ali <ali.mohammed4@yahoo.com>
2021-10-14 18:44:54 +0300

    ignore npm-debug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import Mongoose from "mongoose";
-import productGategorys from './models/productGategorys.js'
+import productGategorys from "./models/productGategorys.js";
 import products from "./models/products.js";
 import bestSellings from "./models/bestSellings.js";
 import bodyParser from "body-parser";
@@ -10,14 +10,13 @@ import bestSellingsRouter from "./routes/bestSelling.js";
 import productGategorysRouter from "./routes/productGategorys.js";
 import AdminBro from "admin-bro";
 import AdminBroExpress from "@admin-bro/express";
-import AdminBroMongoose from '@admin-bro/mongoose'
-import {addImage} from './controllers/products.js'
-import uploadFeature  from '@admin-bro/upload'
+import AdminBroMongoose from "@admin-bro/mongoose";
+import { addImage } from "./controllers/products.js";
+import uploadFeature from "@admin-bro/upload";
 import path from "path";
-import { log } from "console";
-
-
-AdminBro.registerAdapter(AdminBroMongoose)
+import dotenv from 'dotenv'
+dotenv.config()
+AdminBro.registerAdapter(AdminBroMongoose);
 const app = express();
 app.use("/images", express.static("./images"));
 app.use(bodyParser.json());
@@ -29,67 +28,39 @@ app.use(
     credentials: true,
   })
 );
-const Db_URL =
-  "mongodb+srv://ali:ali123@cluster0.qef8w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
+const connection = await Mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+const adminBro = new AdminBro({
+  Database: [connection],
+  rootPath: "/admin",
+  resources: [products, productGategorys, bestSellings],
+});
+const router = AdminBroExpress.buildRouter(adminBro);
 
-
-
-
-
-  const connection = await  Mongoose.connect(Db_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  const adminBro = new AdminBro({
-    Database: [connection],
-    rootPath: '/admin',
-    resources: [
-      products,
-      productGategorys,
-      bestSellings
-    ],
-  
-  })
-  const router = AdminBroExpress.buildRouter (adminBro)
-
-  
-  
-
-
-
-
-
-
-
-  app.use(adminBro.options.rootPath, router)
+app.use(adminBro.options.rootPath, router);
 app.use("/api/product", productRouter);
 app.use("/api/bestSelling", bestSellingsRouter);
 app.use("/api/productGategorys", productGategorysRouter);
 
-app.use(express.static(path.join('client/build')));
-console.log(__dirname , '1');
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.use(express.static(path.join("client/build")));
+console.log(__dirname, "1");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
-console.log(__dirname , '2');
+console.log(__dirname, "2");
 
 app.get("/api", (req, res) => {
   res.send("hello to home");
 });
 
-app.listen(PORT, () => console.log(`AdminBro is under localhost:${PORT}/admin`))
-
-  
-
-
-
-
-
-
-
+app.listen(PORT, () =>
+  console.log(`AdminBro is under localhost:${PORT}/admin`)
+);
