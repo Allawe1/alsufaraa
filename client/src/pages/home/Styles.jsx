4a5fa77dc5641c4905c1f2bf79dab30380ae831a
Ali <ali.mohammed4@yahoo.com>
2021-10-17 18:21:44 +0300

    ignore npm-debug

--- client/src/pages/home/Styles.jsx
+++ client/src/pages/home/Styles.jsx
@@ -5,11 +5,13 @@ export const useStyles = makeStyles((theme) => ({
     paddingTop : 550,
     textAlign : 'center',
     position: "relative",
-     [`${theme.breakpoints.only('sm')} and (orientation: landscape)`]: {
+    [`${theme.breakpoints.only('sm')} and (orientation: landscape)`]: {
       marginTop: -50,
       padding: "50px ",
-      margin : '-50px -50px',
+      margin : '-50px -50px'
+      
      },
+  
   },
   home: {
     justifyContent: 'center',
@@ -22,10 +24,8 @@ export const useStyles = makeStyles((theme) => ({
       padding : '100px 20px',
       
      },
-     [`${theme.breakpoints.only('sm')} and (orientation: landscape)`]: {
-      marginTop: -50,
-      padding: "50px ",
-      margin : '-50px -50px',
+     [theme.breakpoints.only('sm')]: {
+      padding : '100px 50px',
      },
      [theme.breakpoints.only('xs')]: {
       padding : '80px 10px',
