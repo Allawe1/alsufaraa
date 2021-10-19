95300dc107c5f48cd882808b76adf534b588c7cc
Ali <ali.mohammed4@yahoo.com>
2021-10-19 16:54:01 +0300

    ignore npm-debug

--- client/src/pages/productDetails/productDetails.jsx
+++ client/src/pages/productDetails/productDetails.jsx
@@ -6,30 +6,45 @@ import { useMediaQuery } from 'react-responsive'
 export function ProductDetails(props) {
   const classes = useStyles();
   var state = props.location.state;
+  console.log(state);
   if(!state){
       state={}
   }
   const productGategoryName = [
     'Arabic',
     'Tanoor',
-    'cake',
-    'pastry'
+    'Kaak',
+    'pastry',
+    'Bun',
+    'Others',
+    'Toast'
   ]
   var selectedGatogary;
   
   if(state.productGategory==="6144e629e151f5246478a862"){
   selectedGatogary = productGategoryName[0]
+  }else if(state.productGategory==="6144b6a36840c728943b15d6")
+  {
+    selectedGatogary = productGategoryName[1]
   }else if(state.productGategory==="6144b6996840c728943b15d4")
   {
     selectedGatogary = productGategoryName[2]
   }else if(state.productGategory==="6144b6ac6840c728943b15d8")
   {
     selectedGatogary = productGategoryName[3]
-  }else if(state.productGategory==="6144b6a36840c728943b15d6")
+  }else if(state.productGategory==="61698c697fbb8b002352fd2b")
   {
-    selectedGatogary = productGategoryName[1]
+    selectedGatogary = productGategoryName[4]
   }
-  
+  else if(state.productGategory==="61698d5f7fbb8b002352fd30")
+  {
+    selectedGatogary = productGategoryName[5]
+  }
+  else if(state.productGategory==="615f0478d01dc03d889d09d8")
+  {
+    selectedGatogary = productGategoryName[6]
+  }
+  console.log(selectedGatogary);
 
   // const isDesktopOrLaptop = useMediaQuery({ query: '(min-device-width: 1224px)' })
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
