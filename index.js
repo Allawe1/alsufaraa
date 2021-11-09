635fd92d9215793724dd38947a99be471f279841
Ali <ali.mohammed4@yahoo.com>
2021-11-09 15:46:01 +0200

    ignore npm-debug

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.use("/api/productGategorys", productGategorysRouter);
 
 app.use(express.static(path.join("client/build")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
